b77ff61978c4fa9d472095e19fd4594012d98f40
Lalkinski <mariolalkinski@gmail.com>
2026-02-10 12:44:25 +0200

    Add project files.

---
+++ HotelMvc_Project/Data/HotelMvcDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelMvc_Project.Data
+{
+    public class HotelMvcDbContext(DbContextOptions<HotelMvcDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
