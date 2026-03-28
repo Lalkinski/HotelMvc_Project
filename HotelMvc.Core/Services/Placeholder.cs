595870c3f9abdad201e00cdccbe6bda8c349809c
Lalkinski <mariolalkinski@gmail.com>
2026-03-28 10:08:53 +0200

    Add placeholder files to include project folder structure

---
+++ HotelMvc.Core/Services/Placeholder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelMvc.Core.Services
+{
+    public class Placeholder
+    {
+    }
+}
---
+++ HotelMvc.Infrastructure/Data/Placeholder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelMvc.Infrastructure.Data
+{
+    internal class Placeholder
+    {
+    }
+}
