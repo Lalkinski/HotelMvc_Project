dd6f0b73cf03bdf2bd408dabb0e547eb91cd7d0a
Lalkinski <mariolalkinski@gmail.com>
2026-03-31 15:03:06 +0300

    Add hotels controller and index view using hotel service

---
+++ HotelMvc_Project/Controllers/HotelsController.cs
@@ -0,0 +1,22 @@
+﻿using HotelMvc.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelMvc.Web.Controllers;
+
+public class HotelsController : Controller
+{
+    private readonly IHotelService hotelService;
+
+    public HotelsController(IHotelService hotelService)
+    {
+        this.hotelService = hotelService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var hotels = await hotelService.GetAllAsync();
+
+        return View(hotels);
+    }
+}
