10bd7729e72fcd85bd163833687c42596bf3905d
Lalkinski <mariolalkinski@gmail.com>
2026-04-02 19:40:48 +0300

    Add admin hotels management page with area routing and role-based access

---
+++ HotelMvc_Project/Areas/Admin/Controllers/HotelsController.cs
@@ -0,0 +1,24 @@
+﻿using HotelMvc.Core.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelMvc.Web.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = "Administrator")]
+public class HotelsController : Controller
+{
+    private readonly IHotelService hotelService;
+
+    public HotelsController(IHotelService hotelService)
+    {
+        this.hotelService = hotelService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var hotels = await hotelService.GetAllAsync();
+        return View(hotels);
+    }
+}
