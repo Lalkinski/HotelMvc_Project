fb68258c5e537a108240bf38521b7d0eac9ef4f3
Lalkinski <mariolalkinski@gmail.com>
2026-04-03 18:54:42 +0300

    Add admin rooms listing page in admin area

---
+++ HotelMvc_Project/Areas/Admin/Controllers/RoomsController.cs
@@ -0,0 +1,25 @@
+﻿using HotelMvc.Core.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelMvc.Web.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = "Administrator")]
+public class RoomsController : Controller
+{
+    private readonly IRoomService roomService;
+
+    public RoomsController(IRoomService roomService)
+    {
+        this.roomService = roomService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var rooms = await roomService.GetAllAsync();
+
+        return View(rooms);
+    }
+}
